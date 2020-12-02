af077fbf7bc653ee4bc459c207ed3bb109b223b6
“saschakala” <mxsaschakala@gmail.com>
2020-12-01 22:36:46 -0500

    commit tree 4b2eb778c4603330a72211aefec6ee4b675752c4

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Your code here
+
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flair="*") {
+    return function(adj="special"){
+        return `You are ${flair}${adj}${flair}!`
+    }
+}
+
+const Calculator = {
+    add: function(num1, num2){
+        return num1 + num2
+    },   
+
+    subtract: function(num1, num2){
+        return num1 - num2
+    },
+
+    multiply: function(num1, num2){
+        return num1 * num2
+    },
+
+    divide: function(num1, num2){
+        return num1 / num2
+    },
+}
+
+function actionApplyer(startNum, arr) {
+    for (const i in arr) {
+        startNum = arr[i](startNum);
+    }
+    return startNum;
+};
+
